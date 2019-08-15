92fe491ed36b66649525874de1d66e4487d0715e
José Luis Santana <yuniorglez@Mac-Yunior.local>
2019-08-15 09:38:57 +0100

    Creado el componente de TodoList

---
+++ VueJS/todo-list-api-and-vuejs/front/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h2>Mis todos maravillosos</h2>
+    <TodoList :apiEndpoint="apiEndpoint" />
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "app",
+  components: {
+    TodoList
+  },
+  data() {
+    return {
+      apiEndpoint: "http://localhost:2222/api/todos",
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueJS/todo-list-api-and-vuejs/front/src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo-list">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    apiEndpoint: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .todo-list{
+
+  }
+
+</style>
