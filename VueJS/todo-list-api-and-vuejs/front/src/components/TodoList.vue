547eed4d63b5e8636176802ddff0beaad0e6b42f
José Luis Santana <yuniorglez@Mac-Yunior.local>
2019-08-15 09:40:37 +0100

    Preparado los data del todolist component y el evento donde haré el get

--- VueJS/todo-list-api-and-vuejs/front/src/components/TodoList.vue
+++ VueJS/todo-list-api-and-vuejs/front/src/components/TodoList.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="todo-list">
-
+    <input v-model="newTodo.text" type="text" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TodoList',
+  name: "TodoList",
   props: {
     apiEndpoint: String
+  },
+  data() {
+    return {
+      newTodo: {},
+      todos : []
+    };
+  },
+  created() {
+    alert('Me acabo de crear')
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .todo-list{
-
-  }
-
+.todo-list {
+}
 </style>
